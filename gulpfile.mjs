972cdce88489eb5befe06386dbbd97036badffd3
shehab24 <mdshehab204@gmail.com>
2024-03-24 11:09:15 +0600

    added

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,24 +1,24 @@
-import { deleteAsync } from 'del';
+import del from 'del';
 import gulp from 'gulp';
 import zip from 'gulp-zip';
 
-gulp.task('clean', async () => await deleteAsync(['languages', 'bundled']));
+gulp.task('clean', async () => await del(['languages', 'bundled']));
 
 export const bundle = () =>
-	gulp
-		.src([
-			'**/*',
-			'!bundled/**',
-			'!node_modules/**',
-			'!src/**',
-			'!.eslintrc.js',
-			'!.gitignore',
-			'!gulpfile.mjs',
-			'!package.json',
-			'!package-lock.json',
-			'!readme.md',
-			'!todo.txt',
-			'!webpack.config.js',
-		])
-		.pipe(zip('store-finder.zip'))
-		.pipe(gulp.dest('bundled'));
+  gulp
+    .src([
+      '**/*',
+      '!bundled/**',
+      '!node_modules/**',
+      '!src/**',
+      '!.eslintrc.js',
+      '!.gitignore',
+      '!gulpfile.mjs',
+      '!package.json',
+      '!package-lock.json',
+      '!readme.md',
+      '!todo.txt',
+      '!webpack.config.js',
+    ])
+    .pipe(zip('store-finder.zip'))
+    .pipe(gulp.dest('bundled'));
